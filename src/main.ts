f042e7033801c14e4729ba9740d4459b2f987920
Matiboux <matiboux@gmail.com>
2021-11-10 00:10:33 +0100

    Add code to login into the discord-mel bot

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import { Bot } from 'discord-mel'
+
+const bot = new Bot({
+	absPath: __dirname,
+	configFile: '../config.json',
+}, {
+	intents: [
+		Bot.Intents.FLAGS.GUILDS,
+		Bot.Intents.FLAGS.GUILD_MESSAGES,
+		Bot.Intents.FLAGS.DIRECT_MESSAGES,
+	],
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+})
+
+// Start the bot
+bot.start()
