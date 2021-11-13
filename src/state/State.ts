dd413a027e9c320f793fe88fd73f3e0c0b054036
Matiboux <matiboux@gmail.com>
2021-11-13 19:29:02 +0100

    Override initProperties in State

--- src/state/State.ts
+++ src/state/State.ts
@@ -1,16 +1,22 @@
-import { State as BaseState } from 'discord-mel'
+import { BaseState } from 'discord-mel'
 import StateType from './StateType'
 
 class State extends BaseState
 {
-	_db: StateType = new StateType()
+	_db!: StateType
 
-	js: StateType = new StateType()
+	js!: StateType
 
 	constructor(stateFile?: string, charset: BufferEncoding = 'utf8')
 	{
         super(stateFile, charset)
     }
+
+	protected initProperties()
+	{
+		this._db = new StateType()
+		this.js = new StateType()
+	}
 
 	get db()
 	{
