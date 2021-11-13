cbcf18c648f98f1268161d33b0b5b29f626b7f53
Matiboux <matiboux@gmail.com>
2021-11-13 23:46:30 +0100

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
-import { Bot } from 'discord-mel'
 import path from 'path'
+
+import { Bot } from 'discord-mel'
+
 import State from './state/State'
 
 Bot.Services.State = State
 
-const bot = new Bot({
-	absPath: path.dirname(__dirname),
-	configFile: './config.json',
-}, {
-	intents: [
-		Bot.Intents.FLAGS.GUILDS,
-		Bot.Intents.FLAGS.GUILD_MESSAGES,
-		Bot.Intents.FLAGS.DIRECT_MESSAGES,
-	],
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-})
+const bot = new Bot(
+	{
+		absPath: path.dirname(__dirname),
+		configFile: './config.json',
+	},
+	{
+		intents:
+			[
+				Bot.Intents.FLAGS.GUILDS,
+				Bot.Intents.FLAGS.GUILD_MESSAGES,
+				Bot.Intents.FLAGS.DIRECT_MESSAGES,
+			],
+		partials:
+			[
+				'MESSAGE',
+				'CHANNEL',
+				'REACTION'
+			],
+	})
 
 // Start the bot
 bot.start()
