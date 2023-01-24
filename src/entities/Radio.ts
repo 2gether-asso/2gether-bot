bff8d94833b6c6d81544cd0d211f8ed4042a3ede
Matiboux <matiboux@gmail.com>
2023-01-24 17:43:01 +0100

    Update getRadioMessage in Radio

--- src/entities/Radio.ts
+++ src/entities/Radio.ts
@@ -11,45 +11,47 @@ class Radio extends AbstractEntity
 
 	// public radioMessage: Discord.Message
 
-	public radioMessagePromise: Promise<Discord.Message<true>>
+	protected radioMessagePromise: Promise<Discord.Message<true>> | undefined = undefined
 
 	public constructor(bot: Mel, radioData: RadioData)
 	{
 		super(bot)
 
 		this.data = radioData
-
-		// Load radio message
-		this.radioMessagePromise = this.getRadioMessage()
 	}
 
-	protected getRadioMessage(): Promise<Discord.Message<true>>
+	public async getRadioMessage(reload: boolean = false): Promise<Discord.Message<true>>
 	{
-		if (!this.data.messageChannelId)
+		if (reload || !this.radioMessagePromise)
 		{
-			return Promise.reject(new Error('Radio message channel ID not specified'))
+			if (!this.data.messageChannelId)
+			{
+				return Promise.reject(new Error('Radio message channel ID not specified'))
+			}
+
+			this.radioMessagePromise = this.bot.client.channels.fetch(this.data.messageChannelId)
+				.then(channel =>
+					{
+						if (!this.data.messageId)
+						{
+							return Promise.reject(new Error('Radio message ID not specified'))
+						}
+
+						if (!(channel instanceof Discord.GuildChannel) || !channel.isTextBased())
+						{
+							return Promise.reject(new Error('Channel is not a guild text channel'))
+						}
+
+						return channel.messages.fetch({ message: this.data.messageId, force: true })
+					})
+				.catch(error =>
+					{
+						this.bot.logger.warn(`Failed to fetch radio message`, 'Radio', error)
+						return Promise.reject(error)
+					})
 		}
 
-		return this.bot.client.channels.fetch(this.data.messageChannelId)
-			.then(channel =>
-				{
-					if (!this.data.messageId)
-					{
-						return Promise.reject(new Error('Radio message ID not specified'))
-					}
-
-					if (!(channel instanceof Discord.GuildChannel) || !channel.isTextBased())
-					{
-						return Promise.reject(new Error('Channel is not a guild text channel'))
-					}
-
-					return channel.messages.fetch({ message: this.data.messageId, force: true })
-				})
-			.catch(error =>
-				{
-					this.bot.logger.warn(`Failed to fetch radio message`, 'Radio', error)
-					return Promise.reject(error)
-				})
+		return this.radioMessagePromise
 	}
 
 	public isExpired(): boolean
