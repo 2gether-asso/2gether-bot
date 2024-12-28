91efbf36190754273c8cdd7861625c0a0823f5c4
Matiboux <matiboux@gmail.com>
2024-12-28 01:51:54 +0100

    Export configs & use to start bot

--- app/app/src/config.ts
+++ app/app/src/config.ts
@@ -1,31 +1,31 @@
-const DISCORD_TOKEN = import.meta.env.DISCORD_TOKEN || null
+export const DISCORD_TOKEN = import.meta.env.DISCORD_TOKEN || null
 
-const MEL_LOG_PATH = import.meta.env.MEL_LOG_PATH || null
-const MEL_LOG_FILE = import.meta.env.MEL_LOG_FILE || null
-const MEL_LOG_LEVEL = import.meta.env.MEL_LOG_LEVEL || null
+export const MEL_LOG_PATH = import.meta.env.MEL_LOG_PATH || null
+export const MEL_LOG_FILE = ((logFile: string) =>
+	logFile === 'null' ? null : logFile
+)(import.meta.env.MEL_LOG_FILE || './log.txt')
+export const MEL_LOG_LEVEL = import.meta.env.MEL_LOG_LEVEL || null
 
-const MEL_STATE_PATH = ((statePath: string) =>
+export const MEL_STATE_PATH = ((statePath: string) =>
 	statePath === 'null' ? null : statePath
 )(import.meta.env.MEL_STATE_PATH || null)
-
-const MEL_STATE_FILE = ((stateFile: string) =>
+export const MEL_STATE_FILE = ((stateFile: string) =>
 	stateFile === 'null' ? null : stateFile
-)(import.meta.env.MEL_STATE_FILE || 'state.json')
+)(import.meta.env.MEL_STATE_FILE || './state.json')
 
-const MEL_TRANSLATIONS_DIR = ((translationsDir: string) =>
+export const MEL_TRANSLATIONS_DIR = ((translationsDir: string) =>
 	translationsDir === 'null' ? null : translationsDir
 )(import.meta.env.MEL_TRANSLATIONS_DIR || './translations')
+export const MEL_DEFAULT_LANGUAGE = import.meta.env.MEL_DEFAULT_LANGUAGE || 'fr'
 
-const MEL_DEFAULT_LANGUAGE = import.meta.env.MEL_DEFAULT_LANGUAGE || 'fr'
-
-const MEL_COMMANDS_DIR = ((commandsDir: string) =>
+export const MEL_COMMANDS_DIR = ((commandsDir: string) =>
 	commandsDir === 'null' ? null : commandsDir
 )(import.meta.env.MEL_COMMANDS_DIR || './dist/commands')
 
-const MEL_GLOBAL = JSON.parse(import.meta.env.MEL_GLOBAL || '{}')
+export const MEL_GLOBAL = JSON.parse(import.meta.env.MEL_GLOBAL || '{}')
 
-const MEL_GUILD_DEFAULT = JSON.parse(import.meta.env.MEL_GUILD_DEFAULT || '{}')
+export const MEL_GUILD_DEFAULT = JSON.parse(import.meta.env.MEL_GUILD_DEFAULT || '{}')
 
-const MEL_GUILDS = JSON.parse(import.meta.env.MEL_GUILDS || '{}')
+export const MEL_GUILDS = JSON.parse(import.meta.env.MEL_GUILDS || '{}')
 
-const MEL_SERVICES = JSON.parse(import.meta.env.MEL_SERVICES || '{}')
+export const MEL_SERVICES = JSON.parse(import.meta.env.MEL_SERVICES || '{}')
--- app/app/src/main.ts
+++ app/app/src/main.ts
@@ -5,6 +5,7 @@ import { Mel } from 'discord-mel'
 import State from './state/State'
 import ActivityHooks from './hooks/ActivityHooks'
 import Config from './config/Config'
+import * as LocalConfig from './config'
 
 Mel.Services.Config = Config
 Mel.Services.State = State
@@ -12,8 +13,18 @@ Mel.Services.State = State
 const bot = new Mel(
 	{
 		absPath: path.dirname(__dirname),
-		configFile: './config.json',
-		logFile: './log.txt',
+		...(LocalConfig.MEL_LOG_PATH !== null ? { logPath: LocalConfig.MEL_LOG_PATH } : {}),
+		...(LocalConfig.MEL_LOG_FILE !== null ? { logFile: LocalConfig.MEL_LOG_FILE } : {}),
+		...(LocalConfig.MEL_LOG_LEVEL !== null ? { logLevel: LocalConfig.MEL_LOG_LEVEL } : {}),
+		...(LocalConfig.MEL_STATE_PATH !== null ? { statePath: LocalConfig.MEL_STATE_PATH } : {}),
+		...(LocalConfig.MEL_STATE_FILE !== null ? { stateFile: LocalConfig.MEL_STATE_FILE } : {}),
+		...(LocalConfig.MEL_TRANSLATIONS_DIR !== null ? { translationsDir: LocalConfig.MEL_TRANSLATIONS_DIR } : {}),
+		...(LocalConfig.MEL_DEFAULT_LANGUAGE !== null ? { defaultLanguage: LocalConfig.MEL_DEFAULT_LANGUAGE } : {}),
+		...(LocalConfig.MEL_COMMANDS_DIR !== null ? { commandsDir: LocalConfig.MEL_COMMANDS_DIR } : {}),
+		...(LocalConfig.MEL_GLOBAL !== null ? { global: LocalConfig.MEL_GLOBAL } : {}),
+		...(LocalConfig.MEL_GUILD_DEFAULT !== null ? { guildDefault: LocalConfig.MEL_GUILD_DEFAULT } : {}),
+		...(LocalConfig.MEL_GUILDS !== null ? { guilds: LocalConfig.MEL_GUILDS } : {}),
+		...(LocalConfig.MEL_SERVICES !== null ? { services: LocalConfig.MEL_SERVICES }
 	},
 	{
 		intents:
@@ -35,4 +46,4 @@ const bot = new Mel(
 const activityHooks = new ActivityHooks(bot)
 
 // Start the bot
-bot.start()
+bot.start(LocalConfig.DISCORD_TOKEN)
